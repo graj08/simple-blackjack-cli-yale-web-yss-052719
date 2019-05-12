5bb6407e1fa65c3dec2f5b9da791f1dd96befe0e
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-12 18:01:03 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -13,11 +13,11 @@ def display_card_total (card_total)
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"# code #prompt_user here
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets user_input# code #get_user_input here
 end
 
 def end_game
